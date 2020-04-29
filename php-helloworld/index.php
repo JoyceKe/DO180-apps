675504171bbca3c867f90878c56630f52fc63a7a
Student User <student@workstation.lab.example.com>
2020-04-29 18:35:01 +0000

    changed content

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is a coming!\n";?>
+print "A change is a coming!\n";
+print "A second change will be here soon!\n";
+?>
